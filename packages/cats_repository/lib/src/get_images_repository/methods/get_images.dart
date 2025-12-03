65bc76e10e0ae69246f4354246b79e6b79bbc3b0
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-03 16:54:05 +0800

    🐛 為 fetchCatsImages  修改回傳參數為 CatImageEntity List而不是 ImageModel list

--- packages/cats_repository/lib/src/get_images_repository/methods/get_images.dart
+++ packages/cats_repository/lib/src/get_images_repository/methods/get_images.dart
@@ -1,3 +1,4 @@
+import 'package:cats_repository/cats_repository.dart';
 import 'package:public_api/public_api.dart';
 
 class GetImages {
@@ -6,12 +7,20 @@ class GetImages {
   GetImages({required CatApiClient catApiClient})
     : _catApiClient = catApiClient;
 
-  Future<List<ImageModel>> fetchCatsImages(int limit) async => _catApiClient
+  Future<List<CatImageEntity>> fetchCatsImages(int limit) async => _catApiClient
       .fetchCatsImages(limit)
       .then((catList) {
-        List<ImageModel> cats = [];
+        List<CatImageEntity> cats = [];
         for (int i = 0; i < catList.length; i++) {
-          cats.add(catList[i]);
+          final catModel = catList[i];
+          cats.add(
+            CatImageEntity(
+              id: catModel.id,
+              url: catModel.url,
+              urlHeight: catModel.urlHeight,
+              urlWidth: catModel.urlWidth,
+            ),
+          );
         }
         return cats;
       })
