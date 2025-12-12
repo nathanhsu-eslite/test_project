ad29ed619a13a77bc17c91645f87099146b6cbf3
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-12 16:47:23 +0800

    ✨ feat : [repository layer] add favorite

--- packages/cats_repository/lib/src/favorite_repository/methods/interface/interface.dart
+++ packages/cats_repository/lib/src/favorite_repository/methods/interface/interface.dart
@@ -2,3 +2,4 @@ export 'clear_favorite_interface.dart';
 export 'delete_favorite_interface.dart';
 export 'get_all_favorite_interface.dart';
 export 'find_favorite_interface.dart';
+export 'add_favorite_interface.dart';
