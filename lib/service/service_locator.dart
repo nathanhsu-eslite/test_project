1601abddeca73e6eec4ce62d76841cdf0d981595
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-12 17:47:32 +0800

    ✨ feat : [presentation layer] favorite feature

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -1,10 +1,32 @@
+import 'package:data/data.dart';
+import 'package:data/objectbox.g.dart';
 import 'package:dio/dio.dart';
 import 'package:domain/domain.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
-void setupLocator() {
+void setupLocator({required Store store}) {
+  getIt.registerLazySingleton<Store>(() => store);
+  getIt.registerLazySingleton<FavoriteInterface>(
+    () => FavoriteCatDB(store: getIt<Store>()),
+  );
+  getIt.registerLazySingleton<GetAllFavoriteUseCase>(
+    () => GetAllFavoriteUC.create(db: getIt<FavoriteInterface>()),
+  );
+  getIt.registerLazySingleton<FindFavoriteUseCase>(
+    () => FindFavoriteUC.create(db: getIt<FavoriteInterface>()),
+  );
+  getIt.registerLazySingleton<DeleteFavoriteUseCase>(
+    () => DeleteFavoriteUC.create(db: getIt<FavoriteInterface>()),
+  );
+  getIt.registerLazySingleton<ClearFavoriteUseCase>(
+    () => ClearFavoriteUC.create(db: getIt<FavoriteInterface>()),
+  );
+  getIt.registerLazySingleton<AddFavoriteUseCase>(
+    () => AddFavoriteUC.create(db: getIt<FavoriteInterface>()),
+  );
+
   getIt.registerLazySingleton(() => Dio());
   getIt.registerLazySingleton(() => GetCatsImagesUC.dio(dio: getIt<Dio>()));
   getIt.registerLazySingleton(() => GetCatDetailUC.dio(dio: getIt<Dio>()));
