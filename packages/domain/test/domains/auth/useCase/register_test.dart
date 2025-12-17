9b27c321784f68f8b69588356be775b488935022
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-17 16:04:31 +0800

    ✅ test : [domain layer]add unit test for register use case

---
+++ packages/domain/test/domains/auth/useCase/register_test.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+import 'package:data/data.dart';
+import 'package:data/objectbox.g.dart';
+import 'package:domain/src/domains/auth/auth.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late final Box<UserEntity> box;
+  late final AuthDBInterface authDB;
+  late final Store store;
+  late Directory testDir;
+
+  setUpAll(() async {
+    testDir = await Directory.systemTemp.createTemp('objectbox_test_');
+    store = await openStore(directory: testDir.path);
+    box = store.box<UserEntity>();
+    authDB = AuthDB(store: store);
+  });
+  tearDown(() {
+    box.removeAll();
+  });
+  group('RegisterUC', () {
+    late RegisterUseCase registerUC;
+
+    setUp(() {
+      registerUC = RegisterUC.create(db: authDB);
+    });
+
+    test('should call handle on RegisterRepo', () async {
+      // Arrange
+      const userName = 'newUser';
+      const password = 'newPassword';
+
+      await registerUC(userName: userName, password: password);
+      final result = box
+          .query(UserEntity_.username.equals(userName))
+          .build()
+          .find();
+
+      expect(result.first.username, userName);
+    });
+    test(
+      'throw UsernameAlreadyExistsAuthException when username already exists',
+      () async {
+        // Arrange
+        const userName = 'testUser';
+        const password = 'password';
+        box.put(
+          UserEntity(encryptedPassword: password.encrypt(), username: userName),
+        );
+
+        expect(
+          () async => await registerUC(userName: userName, password: password),
+          throwsA(UsernameAlreadyExistsAuthException),
+        );
+      },
+    );
+  });
+}
