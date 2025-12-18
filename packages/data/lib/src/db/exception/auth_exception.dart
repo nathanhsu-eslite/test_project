cd4fad2c39401537f8ccb285e6d0c6f9ba3eede4
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-18 11:36:09 +0800

    ♻️ refactor :[data layer] remove wrong class modifier

--- packages/data/lib/src/db/exception/auth_exception.dart
+++ packages/data/lib/src/db/exception/auth_exception.dart
@@ -1,5 +1,5 @@
 sealed class AuthException implements Exception {}
 
-sealed class UserNotFindAuthException implements AuthException {}
+class UserNotFindAuthException implements AuthException {}
 
-sealed class UsernameAlreadyExistsAuthException implements AuthException {}
+class UsernameAlreadyExistsAuthException implements AuthException {}
