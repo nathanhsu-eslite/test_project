95b7881a9dd45eeccee46a9771bad9572c348fa7
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-18 16:13:00 +0800

    ♻️ refactor : [business layer] added extra in HomeRoute

--- lib/routes/home_route.g.dart
+++ lib/routes/home_route.g.dart
@@ -12,21 +12,26 @@ RouteBase get $homeRoute =>
     GoRouteData.$route(path: '/home', factory: $HomeRoute._fromState);
 
 mixin $HomeRoute on GoRouteData {
-  static HomeRoute _fromState(GoRouterState state) => HomeRoute();
+  static HomeRoute _fromState(GoRouterState state) =>
+      HomeRoute(state.extra as UserEntity?);
+
+  HomeRoute get _self => this as HomeRoute;
 
   @override
   String get location => GoRouteData.$location('/home');
 
   @override
-  void go(BuildContext context) => context.go(location);
+  void go(BuildContext context) => context.go(location, extra: _self.$extra);
 
   @override
-  Future<T?> push<T>(BuildContext context) => context.push<T>(location);
+  Future<T?> push<T>(BuildContext context) =>
+      context.push<T>(location, extra: _self.$extra);
 
   @override
   void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location);
+      context.pushReplacement(location, extra: _self.$extra);
 
   @override
-  void replace(BuildContext context) => context.replace(location);
+  void replace(BuildContext context) =>
+      context.replace(location, extra: _self.$extra);
 }
