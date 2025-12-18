59cbf9cc9ddfd9dd9438b04b42262b0d2de04271
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-18 17:07:03 +0800

    ♻️ refactor : [business layer] manage feature using get_it scope

--- lib/service/service_locator.dart
+++ lib/service/service_locator.dart
@@ -16,10 +16,8 @@ void setupLocator({required Store store}) {
   getIt.registerLazySingleton<AuthDBInterface>(
     () => AuthDB(store: getIt<Store>()),
   );
-  _registerFavoriteService();
   getIt.registerLazySingleton(() => GetCatsImagesUC.dio(dio: getIt<Dio>()));
-  getIt.registerLazySingleton(() => GetCatDetailUC.dio(dio: getIt<Dio>()));
-  getIt.registerLazySingleton(() => GetMatchResultUC.dio(dio: getIt<Dio>()));
+
   getIt.registerLazySingleton<LoginUseCase>(
     () => LoginUC.create(db: getIt<AuthDBInterface>()),
   );
@@ -28,23 +26,37 @@ void setupLocator({required Store store}) {
   );
 }
 
+void setupAuthScope() {
+  //當登入時才能使用的功能
+  getIt.pushNewScope(
+    scopeName: 'logged-in',
+    init: (scope) {
+      _registerFavoriteService();
+      getIt.registerLazySingleton(() => GetCatDetailUC.dio(dio: getIt<Dio>()));
+      getIt.registerLazySingleton(
+        () => GetMatchResultUC.dio(dio: getIt<Dio>()),
+      );
+    },
+  );
+}
+
 void _registerFavoriteService() {
   getIt.registerLazySingleton<FavoriteInterface>(
     () => FavoriteCatDB(store: getIt<Store>()),
   );
-  getIt.registerLazySingleton<GetAllFavoriteUseCase>(
+  getIt.registerFactory<GetAllFavoriteUseCase>(
     () => GetAllFavoriteUC.create(db: getIt<FavoriteInterface>()),
   );
-  getIt.registerLazySingleton<FindFavoriteUseCase>(
+  getIt.registerFactory<FindFavoriteUseCase>(
     () => FindFavoriteUC.create(db: getIt<FavoriteInterface>()),
   );
-  getIt.registerLazySingleton<DeleteFavoriteUseCase>(
+  getIt.registerFactory<DeleteFavoriteUseCase>(
     () => DeleteFavoriteUC.create(db: getIt<FavoriteInterface>()),
   );
-  getIt.registerLazySingleton<ClearFavoriteUseCase>(
+  getIt.registerFactory<ClearFavoriteUseCase>(
     () => ClearFavoriteUC.create(db: getIt<FavoriteInterface>()),
   );
-  getIt.registerLazySingleton<AddFavoriteUseCase>(
+  getIt.registerFactory<AddFavoriteUseCase>(
     () => AddFavoriteUC.create(db: getIt<FavoriteInterface>()),
   );
 }
