3a555d669f5a6df885d9891571b7fffbeffaab28
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-23 15:14:43 +0800

    🚚 file :[repository] move and rename the module file

--- packages/cats_repository/lib/repository_module.dart
+++ packages/cats_repository/lib/repository_module.dart
@@ -1,4 +1,5 @@
 import 'package:injectable/injectable.dart';
+// import 'repository_module.module.dart'; // 這是自動生成的檔案名稱
 
 @InjectableInit.microPackage()
 void initMicroPackage() {}
---
+++ packages/cats_repository/lib/repository_module.module.dart
@@ -0,0 +1,43 @@
+//@GeneratedMicroModule;CatsRepositoryPackageModule;package:cats_repository/src/auth/repository_module.module.dart
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:async' as _i687;
+
+import 'package:cats_repository/cats_repository.dart' as _i956;
+import 'package:cats_repository/src/auth/methods/interface/login_interface.dart'
+    as _i453;
+import 'package:cats_repository/src/auth/methods/interface/register_interface.dart'
+    as _i1012;
+import 'package:cats_repository/src/auth/methods/login.dart' as _i700;
+import 'package:cats_repository/src/auth/methods/register.dart' as _i703;
+import 'package:cats_repository/src/votes/delete_votes/methods/delete_votes.dart'
+    as _i556;
+import 'package:cats_repository/src/votes/delete_votes/methods/delete_votes_interface.dart'
+    as _i588;
+import 'package:cats_repository/src/votes/get_votes/methods/get_votes.dart'
+    as _i791;
+import 'package:cats_repository/src/votes/vote_cat/methods/vote.dart' as _i820;
+import 'package:cats_repository/src/votes/vote_cat/methods/vote_interface.dart'
+    as _i486;
+import 'package:data/data.dart' as _i437;
+import 'package:injectable/injectable.dart' as _i526;
+
+class CatsRepositoryPackageModule extends _i526.MicroPackageModule {
+// initializes the registration of main-scope dependencies inside of GetIt
+  @override
+  _i687.FutureOr<void> init(_i526.GetItHelper gh) {
+    gh.lazySingleton<_i956.GetVotesInterface>(
+        () => _i791.GetVotesRepo(publicApiClient: gh<_i437.PublicApiClient>()));
+    gh.lazySingleton<_i453.LoginInterface>(
+        () => _i700.LoginRepo(db: gh<_i437.AuthDBInterface>()));
+    gh.lazySingleton<_i486.VoteInterface>(
+        () => _i820.VoteRepo(publicApiClient: gh<_i437.PublicApiClient>()));
+    gh.lazySingleton<_i588.DeleteVotesInterface>(
+        () => _i556.DeleteVotesRepo(apiClient: gh<_i437.PublicApiClient>()));
+    gh.lazySingleton<_i1012.RegisterInterface>(
+        () => _i703.RegisterRepo(db: gh<_i437.AuthDBInterface>()));
+  }
+}
