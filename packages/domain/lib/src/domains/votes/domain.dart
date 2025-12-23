88f0f037b7f798fd73aeac8e34c18a9fc4d13f90
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-23 15:31:29 +0800

    ✨ feat : [domain layer] added get_votes use case

--- packages/domain/lib/src/domains/votes/domain.dart
+++ packages/domain/lib/src/domains/votes/domain.dart
@@ -1,3 +1,9 @@
+import 'package:cats_repository/cats_repository.dart';
+
+abstract interface class GetVotesUseCase {
+  Future<List<VotesDataEntity>> call();
+}
+
 abstract interface class DeleteVotesUseCase {
   Future<void> call(int id);
 }
--- packages/domain/lib/src/domains/votes/useCase/use_case.dart
+++ packages/domain/lib/src/domains/votes/useCase/use_case.dart
@@ -1,1 +1,2 @@
+export 'get_votes.dart';
 export 'delete_votes.dart';
