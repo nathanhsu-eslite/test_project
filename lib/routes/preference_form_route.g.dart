e2cdda010de2d318dc2dd5c4a21fd9a49f6e8271
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-26 15:36:19 +0800

    ♻️ refactor : Correctly define subroutes

--- lib/routes/preference_form_route.g.dart
+++ lib/routes/preference_form_route.g.dart
@@ -6,37 +6,7 @@ part of 'preference_form_route.dart';
 // GoRouterGenerator
 // **************************************************************************
 
-List<RouteBase> get $appRoutes => [$matchRoute, $preferenceRoute];
-
-RouteBase get $matchRoute => GoRouteData.$route(
-  path: '/preference/match-result',
-  factory: $MatchRoute._fromState,
-);
-
-mixin $MatchRoute on GoRouteData {
-  static MatchRoute _fromState(GoRouterState state) =>
-      MatchRoute(state.extra as UserPreference);
-
-  MatchRoute get _self => this as MatchRoute;
-
-  @override
-  String get location => GoRouteData.$location('/preference/match-result');
-
-  @override
-  void go(BuildContext context) => context.go(location, extra: _self.$extra);
-
-  @override
-  Future<T?> push<T>(BuildContext context) =>
-      context.push<T>(location, extra: _self.$extra);
-
-  @override
-  void pushReplacement(BuildContext context) =>
-      context.pushReplacement(location, extra: _self.$extra);
-
-  @override
-  void replace(BuildContext context) =>
-      context.replace(location, extra: _self.$extra);
-}
+List<RouteBase> get $appRoutes => [$preferenceRoute];
 
 RouteBase get $preferenceRoute => GoRouteData.$route(
   path: '/preference',
