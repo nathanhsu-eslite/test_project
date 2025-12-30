f27ffa5bc4f65dd0583595ff4c024db50a5262a5
nathanhsu-eslite <nathanhsu@eslite.com>
2025-12-30 14:30:40 +0800

    ♻️ refactor : Use widget to build sub widgets

--- lib/pages/detail_page/widget/detail_property.dart
+++ lib/pages/detail_page/widget/detail_property.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_3_35_7/features/get_cat_detail/models/cat.dart';
+import 'package:test_3_35_7/pages/detail_page/widget/list_tile.dart';
 
 class DetailProperty extends StatelessWidget {
   final Cat cat;
@@ -10,30 +11,22 @@ class DetailProperty extends StatelessWidget {
     return Column(
       spacing: 0.5,
       children: [
-        _listTile(
+        DetailListTile(
           title: 'Origin :',
           value: cat.origin,
           icon: Icons.location_on,
         ),
         const SizedBox(height: 8),
 
-        _listTile(title: 'lifeSpan :', value: cat.lifeSpan, icon: Icons.timer),
-        _listTile(title: 'temperament :', value: '', icon: Icons.favorite),
+        DetailListTile(
+          title: 'lifeSpan :',
+          value: cat.lifeSpan,
+          icon: Icons.timer,
+        ),
+        DetailListTile(title: 'temperament :', value: '', icon: Icons.favorite),
 
         Text(cat.temperament, maxLines: 3, style: TextStyle()),
       ],
-    );
-  }
-
-  Widget _listTile({
-    required String title,
-    required String value,
-    required IconData icon,
-  }) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(title, style: TextStyle(fontWeight: FontWeight.bold)),
-      trailing: Text(value, style: TextStyle(fontWeight: FontWeight.bold)),
     );
   }
 }
